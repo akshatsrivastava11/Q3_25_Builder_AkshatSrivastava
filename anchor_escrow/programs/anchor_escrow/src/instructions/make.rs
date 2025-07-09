a290f0b69a728b63c53ef5dcba66ec2e06ce2c69
akshatsrivastava11 <dragonaticvansh@gmail.com>
2025-07-09 22:56:56 +0530

    escrowInstruction

---
+++ anchor_escrow/programs/anchor_escrow/src/instructions/make.rs
@@ -0,0 +1,78 @@
+use anchor_lang::{prelude::*, system_program::{transfer, Transfer}};
+use anchor_spl::{associated_token::AssociatedToken, token::{transfer_checked, Mint, Token, TokenAccount, TransferChecked}, token_interface::TokenInterface};
+
+use crate::{make, Escrow};
+
+#[derive(Accounts)]
+#[instruction(seed:u64)]
+pub  struct Make<'info>{
+    #[account(mut)]
+    pub maker:Signer<'info>,
+   
+   
+    #[account(
+        mint::token_program=token_program
+    )
+    ]
+    pub mint_a:InterfaceAccount<'info,Mint>,
+     #[account(
+        mint::token_program=token_program
+    )
+    ]
+    pub mint_b:InterfaceAccount<'info,Mint>,
+   
+   
+    #[account(
+        mut,
+        associated_token::mint=mint_a,
+        associated_token::authority=maker,
+        associated_token::token_program=token_program
+    )]
+    pub maker_ata_mint_a:InterfaceAccount<'info,TokenAccount>,
+   
+   #[account(
+    init,
+    payer=maker,
+    space=8+Escrow::INIT_SPACE,
+    seeds=[b"escrow",maker.key().as_ref(),seed.to_le_bytes().as_ref()],
+    bump
+   )]
+   pub escrow:Account<'info,Escrow>,
+
+   #[account(
+    init,
+    payer=maker,
+    associated_token::mint=mint_a,
+    associated_token::authority=escrow,
+    associated_token::token_program=token_program
+   )]
+   pub vault:InterfaceAccount<'info,TokenAccount>,
+   
+    pub system_program:Program<'info,System>,
+    pub token_program:Interface<'info,TokenInterface>,
+    pub associated_token_program:Program<'info,AssociatedToken>
+}
+
+impl<'info>Make<'info>{
+    pub fn initialize(&mut self,amount:u64,seed:u64,bumps:MakeBumps)->Result<()>{
+        self.escrow.set_inner(Escrow { 
+            maker: self.maker.key(), 
+            mint_a: self.mint_a.key(),
+             mint_b: self.mint_b.key(),
+              amount: amount,
+               seed:seed,
+                bump:bumps.escrow 
+             });  
+             Ok(())     
+    }
+    pub fn make_offer(&mut self,amount:u64)->Result<()>{
+        let cpi_program=self.token_program.to_account_info();
+        let cpi_Accounts=TransferChecked{from:self.maker.to_account_info(),to:self.vault.to_account_info(),
+        authority:self.maker.to_account_info(),
+        mint:self.mint_a.to_account_info()
+        };
+        let cpi_context=CpiContext::new(cpi_program, cpi_Accounts);
+        transfer_checked(cpi_context,amount,self.mint_a.decimals)
+    }
+
+}
---
+++ anchor_escrow/programs/anchor_escrow/src/lib.rs
@@ -0,0 +1,18 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("GZm78V2rHQuZfCY1iNhb3jwNfUuWQuf1LdivDdeCkNVP");
+
+#[program]
+pub mod anchor_escrow {
+    use super::*;
+    
+}
