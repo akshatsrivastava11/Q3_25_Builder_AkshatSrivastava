9f3b88d36eff6f269bff2fb52037cca4a5e01b91
akshatsrivastava11 <dragonaticvansh@gmail.com>
2025-07-12 01:11:49 +0530

    anchorammCompleted

--- anchor-amm/programs/anchor-amm/src/lib.rs
+++ anchor-amm/programs/anchor-amm/src/lib.rs
@@ -16,30 +16,17 @@ pub mod anchor_amm {
 
     use super::*;
 
-   pub fn initialize(
-        ctx: Context<Initialize>,
-        seed: u64,
-        fees: u16,
-        authority: Option<Pubkey>,
-    ) -> Result<()> {
-        ctx.accounts.initialize(seed, fees, authority, ctx.bumps)?;
-        Ok(())
+    pub fn initialize(ctx:Context<Initialize>,seed:u64,fees:u16)->Result<()>{
+        ctx.accounts.initialize(seed,fees,ctx.bumps)
     }
-
-    pub fn deposit(
-        ctx: Context<Deposit>,
-        amount: u64,
-        max_x: u64,
-        max_y: u64,
-    ) -> Result<()> {
+    pub fn deposit(ctx:Context<Deposit>,amount:u64,max_x:u64,max_y:u64)->Result<()>{
         ctx.accounts.deposit(amount, max_x, max_y)
     }
-         pub fn withdraw(
-        ctx: Context<Withdraw>,
-        lp_amount: u64,
-        min_x:  u64,
-        min_y: u64,
-    ) -> Result<()> {
-        ctx.accounts.withdraw_token(lp_amount, min_x,min_y)
+    pub fn withdraw(ctx:Context<Withdraw>,amountLp:u64,max_x:u64,max_y:u64)->Result<()>{
+        ctx.accounts.withdraw(amountLp, max_x, max_y)
     }
+    pub fn swap(ctx:Context<Swap>,amount_in:u64,min_out:u64,is_x_in:bool)->Result<()>{
+        ctx.accounts.swap(amount_in, min_out, is_x_in)
+    }
+ 
 }
--- anchor-amm/programs/anchor-amm/src/state/mod.rs
+++ anchor-amm/programs/anchor-amm/src/state/mod.rs
@@ -3,13 +3,12 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(InitSpace)]
 pub struct Config{
+    pub authority:Pubkey,
     pub mint_x:Pubkey,
     pub mint_y:Pubkey,
-    pub authority:Option<Pubkey>,
+    pub mint_lp:Pubkey,
     pub seed:u64,
-    pub bump_config:u8,
-    pub bump_lp:u8,
-    pub locked:bool,
+    pub bump:u8,
+    pub lp_bump:u8,
     pub fees:u16
 }
-
