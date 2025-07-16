3c023345ae1431c4f675f500d75c798def2f5d62
akshatsrivastava11 <dragonaticvansh@gmail.com>
2025-07-17 01:03:56 +0530

    stakingimplemented

--- nft-staking/programs/nft-staking/src/state/user_config.rs
+++ nft-staking/programs/nft-staking/src/state/user_config.rs
@@ -5,7 +5,7 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(InitSpace)]
 pub struct UserConfig{
-       pub token_number_staked:u8,
-       pub points:u8,
-       pub bumps:u8,
+       pub amount_staked:u8,
+       pub points:u32,
+       pub bump:u8,
 }
