563d8bfb1a5c35d6bf82ea119519537d5780c595
akshatsrivastava11 <dragonaticvansh@gmail.com>
2025-07-17 23:23:09 +0530

    addedmetadataImpl

--- anchor-marketplace/programs/anchor-marketplace/src/instructions/list.rs
+++ anchor-marketplace/programs/anchor-marketplace/src/instructions/list.rs
@@ -1,5 +1,6 @@
+
 use anchor_lang::{prelude::*, solana_program::system_instruction::SystemError};
-use anchor_spl::{associated_token::AssociatedToken, token::{transfer_checked, TransferChecked}, token_interface::{Mint,TokenAccount, TokenInterface}};
+use anchor_spl::{associated_token::AssociatedToken, metadata::{MasterEditionAccount, Metadata, MetadataAccount}, token::{transfer_checked, TransferChecked}, token_interface::{Mint,TokenAccount, TokenInterface}};
 
 use crate::{Listing, Marketplace};
 
@@ -23,18 +24,35 @@ pub struct List<'info>{
     pub mint:InterfaceAccount<'info,Mint>,
     #[account(
         mut,
-        associated_token::authority=maker,
         associated_token::mint=mint,
+        associated_token::authority=maker,
     )]
     pub user_mint_ata:InterfaceAccount<'info,TokenAccount>,
+    //metadata stuff for the mint
     #[account(
         init,
         payer=maker,
-        associated_token::authority=listing,
         associated_token::mint=mint,        
+        associated_token::authority=listing,
     )]
     pub vault_ata:InterfaceAccount<'info,TokenAccount>,
+    pub collection:InterfaceAccount<'info,Mint>,
+    #[account(
+        seeds=[b"metadata",metadata_program.key().as_ref(),mint.key().as_ref()],
+        bump,
+        seeds::program=metadata_program.key(),
+        constraint=metadata.collection.as_ref().unwrap().key.as_ref()==collection.key().as_ref(),
+        constraint=metadata.collection.as_ref().unwrap().verified==true
+    )]
+    pub metadata:Account<'info,MetadataAccount>,
+    #[account(
+        seeds=[b"metadata",metadata_program.key().as_ref(),mint.key().as_ref(),b"edition"],
+        bump,
+        seeds::program=metadata_program.key(),
+    )]
+    pub master_edition:Account<'info,MasterEditionAccount>,
     pub system_program:Program<'info,System>,
+    pub metadata_program:Program<'info,Metadata>,
     pub token_program:Interface<'info,TokenInterface>,
     pub associated_token_program:Program<'info,AssociatedToken>
 }
