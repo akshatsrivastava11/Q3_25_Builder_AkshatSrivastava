06770bb1010e2d325932d51b7f1ac4f40d8ef551
akshatsrivastava11 <dragonaticvansh@gmail.com>
2025-07-19 10:27:27 +0530

    TwoTestpassed

--- nft-staking/tests/nft-staking.ts
+++ nft-staking/tests/nft-staking.ts
@@ -1,19 +1,24 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { NftStaking } from "../target/types/nft_staking";
-import { publicKey } from "@coral-xyz/anchor/dist/cjs/utils";
-import { Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+// import { publicKey } from "@coral-xyz/anchor/dist/cjs/utils";
+import { Keypair, LAMPORTS_PER_SOL, PublicKey, Transaction } from "@solana/web3.js";
 import { BN, min } from "bn.js";
 import { SYSTEM_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/native/system";
 import { TOKEN_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/utils/token";
-import {createMint, getOrCreateAssociatedTokenAccount} from '@solana/spl-token'
+import {createMint, getAssociatedTokenAddress, getOrCreateAssociatedTokenAccount} from '@solana/spl-token'
+import {createUmi} from '@metaplex-foundation/umi-bundle-defaults'
+import { createSignerFromKeypair, generateSigner, keypairIdentity, percentAmount,publicKey as umiPublickey } from "@metaplex-foundation/umi";
+import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import { fromWeb3JsKeypair } from "@metaplex-foundation/umi-web3js-adapters"; // ✅ Adapter bridge
+let metadataProgram = new PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
 describe("nft-staking", () => {
   // Configure the client to use the local cluster.
   let provider=anchor.AnchorProvider.env()
   anchor.setProvider(provider)
   let program=anchor.workspace.nftStaking as Program<NftStaking>
   let admin=Keypair.generate()
-
+  
   let user=Keypair.generate()
   let stakeConfig:PublicKey
   let rewardsMint:PublicKey
@@ -22,12 +27,15 @@ describe("nft-staking", () => {
   let fees=5;
   let freezePeriod=10;
   let userConfig:PublicKey
+  // const umi=createUmi(provider.connection.rpcEndpoint)
+  // const keypair=createSignerFromKeypair(user.publicKey)
 
+
+  let stakeAccount:PublicKey;
    let mint: PublicKey;
   let userMintAta: PublicKey;
   let metadata: PublicKey;
   let masterEdition: PublicKey;
-  let metadataProgram = new PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
   let collectionMint:PublicKey;
   before(async()=>{
     //airdropping to the user and admin
@@ -55,37 +63,9 @@ describe("nft-staking", () => {
     )[0]
 
 
-    //mint
-        mint = await createMint(
-      provider.connection,
-      admin,
-      user.publicKey,
-      user.publicKey,
-      0
-    );
-    //create's user's ata
-    collectionMint=mint
-    userMintAta=(await getOrCreateAssociatedTokenAccount(provider.connection,user,mint,user.publicKey)).address;
 
-    metadata=await PublicKey.findProgramAddressSync(
-      [
-        Buffer.from("metadata"),
-        metadataProgram.toBuffer(),
-        mint.toBuffer()
-      ],
-      metadataProgram
-    )[0];
-    masterEdition=await PublicKey.findProgramAddressSync(
-      [
-        Buffer.from("metadata"),
-        metadataProgram.toBuffer(),
-        mint.toBuffer(),
-        Buffer.from("master")
-      ],
-      metadataProgram
-    )[0]
+    })
 
-  })
 
 
 
@@ -111,9 +91,11 @@ describe("nft-staking", () => {
         }
       ).signers([user]).rpc()
     })
-    it("stake",async()=>{
-      await program.methods.stake()
-    })
+
+
+ 
+
 
 
 });
+
